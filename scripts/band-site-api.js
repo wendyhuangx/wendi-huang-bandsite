dbae8eca71374faf569e05e162efc809290a42b7
“Wendi <wendi.huangg@gmail.comgit config user.namegit config user.emailgit config --global init.defaultBranch main git config --global pull.rebase falsegit config --global user.email wendi.huangg@gmail.com>
2024-10-18 14:15:06 -0700

    setting up api

---
+++ scripts/band-site-api.js
@@ -0,0 +1,47 @@
+class BandSiteApi {
+    constructor(apiKey) {
+        this.apiKey = apiKey;
+        this.baseUrl = 'https://unit-2-project-api-25c1595833b2.herokuapp.com/';
+        this.headers = {
+            'Authorization': `Bearer ${this.apiKey}`,
+            'Content-Type': 'application/json',
+        };
+    }
+
+    //Asynchronous method to post a comment
+    async postComment(commentObj){
+        try{
+            const response = await axios.post(`${this.baseUrl}/comments`, commentObj, {
+                headers:this.headers
+            });
+
+        }catch (error){
+            console.error('Error posting comment:', error);
+        }
+    }
+
+    async getComments(){
+        try{
+            const response = await axios.get(`${this.baseUrl}/comments`,{
+                headers:this.headers
+            });
+            const comments = response.data;
+
+            return comments.sort((a,b) => new Date(b.timestamp)- new Date(a.timestamp));
+        }catch (error){
+            console.error('Error fetching comments:', error);
+        }
+    }
+
+    async getShows(){
+        try{
+            const response= await axios.get(`${this.baseUrl}/shows`, {
+                headers:this.headers
+            });
+            return response.data;
+        }catch{
+            console.error('Error fetching shows:', error);
+        }
+    }
+}
+const api = new BandSiteApi('98b12e9f-9a99-4479-864a-d50f2fc9a7dc');
